0d5171596dd6e09dcf6152405fd7062ead636a1f
Scott Regenthal <scottregenthal@gmail.com>
2019-12-09 11:18:56 -0700

    Record cassette

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,6 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.filter_sensitive_data('<POTTER_API_KEY>') { ENV['POTTER_API_KEY'] }
+  config.configure_rspec_metadata!
 end
